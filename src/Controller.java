a2f366c165dde6b8fe3e8684647b12a3a780845f
Klaraaorban <klara.orban@stud.ubbcluj.ro>
2025-02-18 11:11:42 +0200

    filter by  product universum - d

--- src/Controller.java
+++ src/Controller.java
@@ -18,6 +18,19 @@ public class Controller {
                 .forEach(System.out::println);
     }
 
+    //Anzeige von Charakteren, die Produkte aus einem bestimmten Universum gekauft haben (0.5 Punkte)
+//Der Benutzer gibt ein Universum ein.
+//Die Anwendung zeigt alle Charaktere an, die Produkte aus diesem Universum gekauft haben.
+//Sortierung: Alphabetisch nach Namen (aufsteigend).
+    public void filterByProductUniversum(String universum) {
+        charakterenRepo.getAll().stream()
+                .filter(charakteren -> charakteren.getProduktens().stream()
+                        .anyMatch(produkten -> produkten.getUniversum().equals(universum)))
+                .sorted((c1, c2) -> c1.getName().compareTo(c2.getName()))
+                .forEach(System.out::println);
+    }
 
-}
+
+
+    }
 
